e9be33756a3dc2e0b3586a3738378916675640da
Yjaden Wood <wood.yjaden@gmail.com>
2021-10-31 23:27:59 -0500

    tidying up

--- gg_api.py
+++ gg_api.py
@@ -21,7 +21,7 @@ NAME_DICT = {}
 DEFAULT_YEAR = '2013'
 AWARD_TWEETS = {}
 PRESENTER_TWEETS = []
-PRESENTER_AWARD_TWEETS = []
+PRESENTER_AWARD_TWEETS = {}
 HOST_TWEETS = []
 NOMINEE_TWEETS = []
 PERSON_WORDS = ['actor', 'actress', 'director', 'screenplay', 'cecil']
@@ -126,6 +126,8 @@ def split_award_tweets():
     AWARD_TWEETS["song"] = best_song_tweets
     best_miniseries_tweets = list(filter(lambda x : ('mini-series' in x.lower()), tweets))
     AWARD_TWEETS["mini-series"] = best_miniseries_tweets
+    AWARD_TWEETS["series"] = best_miniseries_tweets
+
 
 # Split Presenter Tweets from TWEETS
 def split_presenter_tweets():
@@ -136,7 +138,37 @@ def split_presenter_tweets():
     PRESENTER_TWEETS = set(presenter_tweets + presenter_tweets_2)
     presenter_award_tweets = [award_pattern_1.search(tweet).group(0)[:-1] for tweet in PRESENTER_TWEETS if award_pattern_1.search(tweet)]
     presenter_award_tweets_2 = [award_pattern_1.search(tweet).group(0)[:-1] for tweet in PRESENTER_TWEETS if award_pattern_1.search(tweet)]
-    PRESENTER_AWARD_TWEETS = set(presenter_award_tweets + presenter_award_tweets_2)
+    tweets = set(presenter_award_tweets + presenter_award_tweets_2)
+    # CECIL AWARD
+    cecil_award_tweets = list(filter(lambda x : ('cecil' in x.lower()), tweets))
+    PRESENTER_AWARD_TWEETS["cecil"] = cecil_award_tweets
+    # BEST ACTRESS
+    best_actress_tweets = list(filter(lambda x : ('actress' in x.lower()), tweets))
+    PRESENTER_AWARD_TWEETS["actress"] = best_actress_tweets
+    # BEST ACTOR
+    best_actor_tweets = list(filter(lambda x : ('actor' in x.lower()), tweets))
+    PRESENTER_AWARD_TWEETS["actor"] = best_actor_tweets
+    # BEST DRAMA | DIRECTOR | SCREENPLAY | ANIMATED | COMEDY, MUSICAL | FOREIGN | SCORE | SONG
+    best_drama_tweets = list(filter(lambda x : ('drama' in x.lower()), tweets))
+    PRESENTER_AWARD_TWEETS["drama"] = best_drama_tweets
+    best_director_tweets = list(filter(lambda x : ('director' in x.lower()), tweets))
+    PRESENTER_AWARD_TWEETS["director"] = best_director_tweets
+    best_screenplay_tweets = list(filter(lambda x : ('screenplay' in x.lower()), tweets))
+    PRESENTER_AWARD_TWEETS["screenplay"] = best_screenplay_tweets
+    best_animated_tweets = list(filter(lambda x : ('animated' in x.lower()), tweets))
+    PRESENTER_AWARD_TWEETS["animated"] = best_animated_tweets
+    best_comedy_tweets = list(filter(lambda x : ('comedy' in x or 'musical' in x.lower()), tweets))
+    PRESENTER_AWARD_TWEETS["comedy"] = best_comedy_tweets
+    best_foreign_tweets = list(filter(lambda x : ('foreign' in x.lower()), tweets))
+    PRESENTER_AWARD_TWEETS["foreign"] = best_foreign_tweets
+    best_score_tweets = list(filter(lambda x : ('score' in x.lower()), tweets))
+    PRESENTER_AWARD_TWEETS["score"] = best_score_tweets
+    best_song_tweets = list(filter(lambda x : ('song' in x.lower()), tweets))
+    PRESENTER_AWARD_TWEETS["song"] = best_song_tweets
+    best_miniseries_tweets = list(filter(lambda x : ('mini-series' in x.lower()), tweets))
+    PRESENTER_AWARD_TWEETS["mini-series"] = best_miniseries_tweets
+    PRESENTER_AWARD_TWEETS["series"] = best_miniseries_tweets
+
 # Split Nominee Tweeets from TWEETS
 def split_nominee_tweets():
     global NOMINEE_TWEETS
@@ -435,11 +467,76 @@ def get_winner(year):
     return winners
 
 def get_presenters(year):
-    '''Presenters is a dictionary with the hard coded award
-    names as keys, and each entry a list of strings. Do NOT change the
-    name of this function or what it returns.'''
-    keywords = PRESENTER_KEYWORDS.split(";")
-    # Your code here
+    # '''Presenters is a dictionary with the hard coded award
+    # names as keys, and each entry a list of strings. Do NOT change the
+    # name of this function or what it returns.'''
+    # keywords = PRESENTER_KEYWORDS.split(";")
+    # count = 0
+    # # Get awards
+    # official_awards = OFFICIAL_AWARDS_1315 if int(year) < 2016 else OFFICIAL_AWARDS_1819
+    # # Winner Dict from Awards
+    # # Initialize values as an empty str
+    # presenters = dict.fromkeys(official_awards, "")
+    # flag = ''
+    # # Loop through awards
+    # for award in official_awards:
+    #     # List of people
+    #     people = []
+    #     # List of nouns
+    #     noun_list = []
+    #     # Get award keywords
+    #     award_keywords = award.split() 
+    #     print(award_keywords)
+    #     # Super HACKY
+    #     award_tweets_key = award_keywords[TOKEN_AWARD_1[count]] if int(year) < 2016 else award_keywords[TOKEN_AWARD_2[count]]
+    #     award_keywords = remove_stopwords_lst(award_keywords)
+    #     # Check if looking for Noun or Person
+    #     if any(word in award_keywords for word in PERSON_WORDS):
+    #         flag = 'PERSON'
+    #         for tweet in AWARD_TWEETS[award_tweets_key]:
+    #             text = tweet
+    #             # Get text
+    #             text.replace('\d+', '')
+    #             text = re.sub(r'\w*\d+\w*', '',text)
+    #             text = re.sub(r'[^\w\s]', '',text)
+    #             # Check for keywords
+    #             if any(word in text.lower() for word in award_keywords):
+    #                 # If so,
+    #                 entities = spacy_tag_entity(text)
+    #                 persons = get_people_from_ents(entities)
+    #                 persons = remove_stopwords_lst(persons, stop_words = STOP_WORDS)
+    #                 if award_tweets_key in persons:
+    #                     persons.remove(award_tweets_key)
+    #                 people = people + persons
+    #     else:
+    #         flag = 'NOUN'
+    #         # Loop through tweets
+    #         for tweet in AWARD_TWEETS[award_tweets_key]:
+    #             text = tweet
+    #             # Get text
+    #             text.replace('\d+', '')
+    #             text = re.sub(r'\w*\d+\w*', '',text)
+    #             text = re.sub(r'[^\w\s]', '',text)
+    #             # Check for keywords
+    #             if any(word in text.lower() for word in award_keywords):
+    #                 # If so,
+    #                 tags = spacy_tag_speech(text)
+    #                 #proper_nouns = get_proper_nouns(tags)
+    #                 nouns = get_proper_nouns(tags)
+    #                 if (type(nouns) != type(None)):
+    #                     nouns = remove_stopwords_lst(nouns, stop_words=STOP_WORDS)
+    #                     if award_tweets_key in nouns:
+    #                         nouns.remove(award_tweets_key)
+    #                     noun_list = noun_list + nouns
+    #     # Get Counter        
+    #     if flag == "PERSON":
+    #         award_counter = Counter(people) 
+    #     if flag == "NOUN":
+    #          award_counter = Counter(noun_list)
+    #     # Get most common element
+    #     presenters[award] = award_counter.most_common(2)
+    #     print(presenters[award])
+    #     count += 1
     return presenters
 
 def pre_ceremony():
@@ -465,11 +562,11 @@ def main():
     split_award_tweets()
     split_nominee_tweets()
     split_presenter_tweets()
-    #HOST_TWEETS = list(filter(lambda x: ('host' in x), TWEETS_LIST))
-    #get_hosts(DEFAULT_YEAR)
-    #get_best_dressed(DEFAULT_YEAR)
-    #get_worst_dressed(DEFAULT_YEAR)
+    get_hosts(DEFAULT_YEAR)
+    get_best_dressed(DEFAULT_YEAR)
+    get_worst_dressed(DEFAULT_YEAR)
     get_winner(DEFAULT_YEAR)
+    get_presenters(DEFAULT_YEAR)
     time_passed = str(time.time() - timer)
     print(f"Time taken: {time_passed}")
     return
